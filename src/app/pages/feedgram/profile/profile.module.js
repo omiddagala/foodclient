51f7db29ee498821da7f7c18958e14aaa7a389d9
root <omid.javaheri66@gmail.com>
2019-04-14 17:31:25 +0430

    feedgram profile

--- src/app/pages/feedgram/profile/profile.module.js
+++ src/app/pages/feedgram/profile/profile.module.js
@@ -20,9 +20,15 @@
             });
     }
 
-    function feedgramProfileCtrl($scope, $compile, $uibModal, baProgressModal, $http, localStorageService, $parse, $rootScope, $state) {
+    function feedgramProfileCtrl($scope, $compile, $uibModal, baProgressModal, $http, localStorageService, $parse, $rootScope, $ionicModal) {
         $rootScope.pageTitle = 'پروفایل';
         $rootScope.currentMobileActiveMenu = "feedgram-profile";
+        $ionicModal.fromTemplateUrl('app/pages/feedgram/profile/edit.html', {
+            scope: $scope,
+            animation: 'slide-in-up',
+        }).then(function (modal) {
+            $scope.editModal = modal;
+        });
 
         setTimeout(function () {
             $scope.loadProfile();
@@ -45,6 +51,25 @@
                 $rootScope.handleError(params, "/feedgram/employee/getPageInfo", err, httpOptions);
             });
         };
+
+        $scope.doEdit = function () {
+            startLoading();
+            var token = localStorageService.get("my_access_token");
+            var httpOptions = {
+                headers: {'Content-type': 'application/json; charset=utf-8', 'Authorization': 'Bearer ' + token}
+            };
+            var params = {
+                "value": $("#desc").val()
+            };
+            $http.post("http://127.0.0.1:9000/v1/feedgram/employee/editPageInfo", params, httpOptions)
+                .success(function (data, status, headers, config) {
+                    $scope.editModal.hide();
+                    $scope.info.description = params.value;
+                    stopLoading();
+                }).catch(function (err) {
+                $rootScope.handleError(params, "/feedgram/employee/editPageInfo", err, httpOptions);
+            });
+        }
 
     }
 })();
